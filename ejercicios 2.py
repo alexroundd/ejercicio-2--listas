a47493f804d1bdfb63d64cf45036e7fc990d65e8
alexroundd <116881330+alexroundd@users.noreply.github.com>
2022-11-11 11:41:24 +0100

    Create ejercicios 2.py
    ejercicio-2; listas python

---
+++ ejercicios 2.py
@@ -0,0 +1,12 @@
+lista = ['casa', 'pene', 'droga', 'asco']
+
+tupla = ('valo', 'csgo', 'minectraft', 'fortnite')
+
+diccionario_alex = {
+    'nombre': 'Alex',
+    'Edad': 18,
+    'Deporte': 'Balonmano',
+    'Enemigo': 'mi padre'
+}
+
+set = set (['dos fardos', 'granada', 'rumba', 'rocket'])
